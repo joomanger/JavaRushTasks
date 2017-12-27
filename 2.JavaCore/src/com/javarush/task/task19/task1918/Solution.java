b89e87ed16ff64f86b6e5c25ffed1dde5e8d6782
savin <savinalexey@gmail.com>
2017-12-27 20:46:39 +0200

    1234

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -12,41 +12,49 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = "c:/apps/test.txt";//console.readLine();
+        String fileName = //"c:/apps/test.txt";
+                console.readLine();
         console.close();
-        String start_tag = "tag";
-        String end_tag = "/" + start_tag;
+
         int new_tag = 0;
+        StringBuffer sbb = new StringBuffer();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            StringBuffer bf = new StringBuffer();
             while (br.ready()) {
-                String line = br.readLine();
-                line=line.replaceAll("\n","");
-                StringTokenizer st = new StringTokenizer(line, "<");
-                while (st.hasMoreElements()) {
-                    //System.out.println(st.nextElement());
-                    String ln = st.nextElement().toString();
-                    if (ln.startsWith(start_tag)) {
-                        new_tag++;
-                        bf.append("<");
-                        bf.append(ln);
-                    }
-                    if (ln.startsWith(end_tag)) {
-                        new_tag--;
-                        if (new_tag == 0) {
-                            bf.append("<");
-                            bf.append(ln);
-                            System.out.println(bf.toString());
-                            bf = new StringBuffer();
-                        }
-                    }
-                    if (!ln.startsWith(start_tag) && !ln.startsWith(end_tag) && new_tag > 0) {
-                        bf.append("<");
-                        bf.append(ln);
-                    }
+                sbb.append(br.readLine());
+            }
+        }
+
+        String tag = args[0];
+        StringBuffer bf = new StringBuffer();
+        String start_tag1 = tag + " ";
+        String start_tag2 = tag + ">";
+        String end_tag = "/" + tag + ">";
+        String line = sbb.toString();
+        StringTokenizer st = new StringTokenizer(line, "<");
+        while (st.hasMoreElements()) {
+            String ln = st.nextElement().toString();
+            if (ln.startsWith(start_tag1) || ln.startsWith(start_tag2)) {
+                new_tag++;
+                bf.append("<");
+                bf.append(ln);
+            }
+            if (ln.startsWith(end_tag)) {
+                new_tag--;
+                if (new_tag == 0) {
+                    bf.append("<");
+                    bf.append(ln);
+                    System.out.println(bf.toString());
+                    bf.delete(0, bf.capacity());
+                    //bf = new StringBuffer();
+                } else {
+                    bf.append("<");
+                    bf.append(ln);
                 }
             }
-
+            if (!ln.startsWith(start_tag1) && !ln.startsWith(start_tag2) && !ln.startsWith(end_tag) && new_tag > 0) {
+                bf.append("<");
+                bf.append(ln);
+            }
         }
     }
 }
