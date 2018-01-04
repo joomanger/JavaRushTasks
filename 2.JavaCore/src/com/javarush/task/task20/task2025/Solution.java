25a30b2d6f87b046f2ec41f82e26a6ab35dd2f89
savin <savinalexey@gmail.com>
2018-01-04 21:01:56 +0200

    1234

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task20.task2025;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
 
 /*
 Алгоритмы-числа
@@ -13,20 +16,27 @@ public class Solution {
         System.out.println("N=" + N);
         //System.out.println(Long.toString(N).length());
         for (long i = 0; i <= N; i++) {
-            byte[] bytes = Long.toString(i).getBytes();
+//            IntStream bytes = Long.toString(i).chars();
+//            int[] ii = bytes.map(new IntUnaryOperator() {
+//                @Override
+//                public int applyAsInt(int i) {
+//                    return i;
+//                }
+//            }).toArray();
+            int[] digits = Long.toString(i).chars().map(c -> c-='0').toArray();
             long res = 0;
             int pw = Long.toString(i).length();
-            for (byte b : bytes) {
-                res += Math.pow(Character.getNumericValue((char) b), pw);
+            for (int f : digits) {
+                res += Math.pow( f, pw);
             }
             if (res == i) {
                 list.add(i);
             }
         }
 
-        for (long ll : list) {
-            System.out.println(ll);
-        }
+//        for (long ll : list) {
+//            System.out.println(ll);
+//        }
 
         //result = list.toArray();
         //System.out.println("res=" + res);
@@ -34,6 +44,18 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        long time = Calendar.getInstance().getTimeInMillis();
         System.out.println(getNumbers(10000000));
+        // System.out.println((byte)5);
+        IntStream bytes = Long.toString(370).chars();
+        int[] ii = bytes.map(new IntUnaryOperator() {
+            @Override
+            public int applyAsInt(int i) {
+                return i;
+            }
+        }).toArray();
+        System.out.println((char) ii[0] + " " + (char) ii[1]);
+
+        System.out.println(Calendar.getInstance().getTimeInMillis() - time);
     }
 }
