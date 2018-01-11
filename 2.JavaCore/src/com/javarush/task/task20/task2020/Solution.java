ce67289524c58d6219576f597bcb5c370ed39f55
savin <savinalexey@gmail.com>
2018-01-11 22:59:28 +0200

    1234

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task20.task2020;
+
+import java.io.*;
+import java.util.logging.Logger;
+
+/* 
+Сериализация человека
+*/
+public class Solution {
+
+    public static class Person implements Serializable {
+        String firstName;
+        String lastName;
+        transient String fullName;
+        transient final String greetingString;
+        String country;
+        Sex sex;
+        transient PrintStream outputStream;
+        transient Logger logger;
+
+        Person(String firstName, String lastName, String country, Sex sex) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.fullName = String.format("%s, %s", lastName, firstName);
+            this.greetingString = "Hello, ";
+            this.country = country;
+            this.sex = sex;
+            this.outputStream = System.out;
+            this.logger = Logger.getLogger(String.valueOf(Person.class));
+        }
+    }
+
+    enum Sex {
+        MALE,
+        FEMALE
+    }
+
+    public static void main(String[] args) throws Exception {
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(arrayOutputStream);
+        Person person = new Person("Alex","Savin","Ukraine",Sex.MALE);
+        oos.writeObject(person);
+
+        ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(arrayOutputStream.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(arrayInputStream);
+
+        Person newPerson = (Person) ois.readObject();
+        System.out.println(newPerson.firstName);
+        System.out.println(newPerson.lastName);
+        System.out.println(newPerson.country);
+        System.out.println(newPerson.sex);
+        System.out.println(newPerson.greetingString);
+       // System.out.println(newPerson.firstName);
+    }
+}
